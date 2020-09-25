5d1960f42d9fb7c0ed8905e0059ce0481381cab6
lih <v_buglh@outlook.com>
2020-09-25 17:45:51 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,71 @@
-module.exports = {
+module.exports = ctx => ({
   base: '/',
-  title: 'Hello VuePress',
+  title: 'Tinf - 天枫前端',
   description: 'Just playing around',
-  dest: 'dist'
-}
+  dest: 'dist',
+  smoothScroll: true,
+  markdown: {
+    lineNumbers: false
+  },
+  themeConfig: {
+    logo: '/logo.png',
+    lastUpdated: 'Last Updated', // string | boolean
+    navbar: true,
+    nav: [
+      { text: 'Article', link: '/article/' },
+      { text: 'Interview', link: '/interview/' },
+      { text: 'Guide', link: '/guide/' },
+      {
+        text: 'Contact',
+        items: [
+          { text: 'bilibili', link: 'https://space.bilibili.com/12445328' }
+        ]
+      },
+      { text: 'Github', link: 'https://github.com/tinfengyee' },
+    ],
+    sidebarDepth: 1,
+    sidebar: {
+      '/interview/': [
+        '',
+        {
+          title: 'javascript',
+          children: [
+            'javascript/javascript'
+          ]
+        },
+        {
+          title: 'vue',
+          children: [
+            'vue/vue'
+          ]
+        },
+        {
+          title: 'h5c3',
+          children: [
+            'h5c3/h5c3'
+          ]
+        }
+      ],
+      '/article/': [
+        '',
+        {
+          title: 'javascript',
+          children: [
+            'javascript/javascript',
+            'javascript/webapi',
+          ]
+        },
+        {
+          title: 'vue',
+          children: [
+            'vue/vue'
+          ]
+        }
+      ],
+      '/guide/': [
+        ''
+      ]
+    }
+  }
+})
+
---
+++ docs/article/javascript/javascript.md
@@ -0,0 +1,23 @@
+# javascript
+
+## 一层
+
+## 二层
+
+```bash
+vuepress
+```
+
+### 2.1 二点一
+
+```js
+let sum = 0;
+
+sum+=1;
+
+console.log(sum);
+```
+
+### 2.2 二点二
+
+#### 三层
